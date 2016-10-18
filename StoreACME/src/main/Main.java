1a7b0320209f54c79b6c9c4ff8590a50303bace5
BLABPC23 <BLABPC23@172.16.7.211>
2016-10-18 13:54:57 -0500

    OLA

--- StoreACME/src/main/Main.java
+++ StoreACME/src/main/Main.java
@@ -15,20 +15,29 @@ public class Main {
     ViewProveedores viewProveedores;
     ControllerProveedores controllerProveedores;
     
+    ModelProductos modelProductos;
+    ViewProductos viewProductos;
+    ControllerProductos controllerProductos;
+    
+    
     public static void main ( String [] amh) {
         
         ModelClientes modelClientes = new ModelClientes();
         ViewClientes viewClientes = new ViewClientes();
         ControllerClientes controllerClientes = new ControllerClientes(viewClientes,modelClientes);
         
+        ModelProductos modelProductos = new ModelProductos();
+        ViewProductos viewProductos = new ViewProductos();
+        ControllerProductos controllerProductos = new ControllerProductos( viewProductos , modelProductos);
+        
         ModelProveedores modelProveedores = new ModelProveedores();
         ViewProveedores viewProveedores = new ViewProveedores();
         
-        JPanel views [] = new JPanel [2];
+        JPanel views [] = new JPanel [3];
         
         views [0] = viewClientes;
         views [1] = viewProveedores;
-        
+        views [2] = viewProductos;
         ViewMain viewMain = new ViewMain();
         ControllerMain controllerMain = new ControllerMain(viewMain, views);
     }  
