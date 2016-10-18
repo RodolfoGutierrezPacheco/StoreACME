2736b4111b81d52a29478838d3e2aff4e53adb4c
BLABPC23 <BLABPC23@172.16.7.211>
2016-10-18 13:17:36 -0500

    OLA

---
+++ StoreACME/src/models/ModelProductos.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author BLABPC23
+ */
+public class ModelProductos {
+    private String producto;
+    private String marca;
+    private String descripcion;
+    private int precio_compra;
+    private String precio_venta;
+    private String productos_existentes;
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPrecio_compra() {
+        return precio_compra;
+    }
+
+    public void setPrecio_compra(int precio_compra) {
+        this.precio_compra = precio_compra;
+    }
+
+    public String getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(String precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+
+    public String getProductos_existentes() {
+        return productos_existentes;
+    }
+
+    public void setProductos_existentes(String productos_existentes) {
+        this.productos_existentes = productos_existentes;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
